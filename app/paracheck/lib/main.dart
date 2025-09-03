8b54accf41ea99555d8064baf53387c8f2e06f44
Leonardo <leonardo.correiamendes.pro@gmail.com>
2025-09-03 15:32:53 +0200

    Hellow world msg + build

--- app/paracheck/lib/main.dart
+++ app/paracheck/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Paracheck',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Paracheck Home Page'),
     );
   }
 }
@@ -109,6 +109,7 @@ class _MyHomePageState extends State<MyHomePage> {
               '$_counter',
               style: Theme.of(context).textTheme.headlineMedium,
             ),
+            const Text('Hello World'),
           ],
         ),
       ),
