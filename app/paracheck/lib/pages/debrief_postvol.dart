56058f73e5ada4084ee6ae3030856be89727aeb3
Diego PB <150288516+Diego-PB@users.noreply.github.com>
2025-09-10 10:39:27 +0200

    base fichier

---
+++ app/paracheck/lib/pages/debrief_postvol.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(DebriefPostVolPage());
+}
+
+class DebriefPostVolPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Worldddd'),
+        ),
+        body: Center(
+          child: Text('Hello, Worldddd!'),
+        ),
+      ),
+    );
+  }
+}
